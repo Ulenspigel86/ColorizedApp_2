e98c53e1a13460c4bd91ccb53c8b8560fc1e3b74
Ulenspigel86 <66313198+Ulenspigel86@users.noreply.github.com>
2024-10-14 16:25:20 +0300

    Initial Commit

---
+++ ColorizedApp_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp_1
+//
+//  Created by Артем Иванов on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
